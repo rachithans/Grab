13e787e859ef3b4a105b5a8c52aa3d754424b873
Benjamin Murray <benjamin.murray@dal.ca>
2021-06-17 18:06:54 -0300

    removed quotes from string

--- Plug_In_Project/video_urls.js
+++ Plug_In_Project/video_urls.js
@@ -8,16 +8,24 @@ var videoRegex = videoType.join("|");
 
 // pattern to match urls that point to videos
 //code adapted from  https://github.com/dimofte/chrome-video-downloader/blob/master/scrape_video_urls.js
-var urlRegex = new RegExp('("|\')(http|https):\\/\\/[^("|\'|,)]*?\\.(' + videoRegex + ")[^(\"|\')]*?(\"|\')", "gi");
+var urlRegex  = new RegExp('("|\')(http|https):\\/\\/[^("|\'|,)]*?\\.(' +videoRegex +')[^("|\')]*?("|\')','gi');
 
-// Build  array of urls by matching with regex
+
+// Build the array of video urls
 var urls = htmlText.match(urlRegex);
-
-// remove quotes from the matched URLS
-var fileRegex = new RegExp('[^\/]+?\.(' + videoRegex + ")", "i");
-//map the URLs to the array.
+// remove quotes from strings
+urls = urls.map(function(quotes) {
+      return quotes.substr(1, quotes.length-2)
+    });
+  
+var fileRegex = new RegExp('[^\/]+?\.(' + videoRegex + ')');
+    
 var vidData = urls.map(function(url) {
-  return {name: url.match(fileRegex)}
+  
+  return {
+    name: url.match(fileRegex)[0],
+    url: url
+  }
 });
-//send data
+
 chrome.extension.sendRequest(vidData);

74f65d33fbec4510152bc3d6b63b56a7e21b573a
Benjamin Murray <benjamin.murray@dal.ca>
2021-07-20 16:01:00 -0300

    added download all button and updated download icon

--- Plug_In_Project/popup.js
+++ Plug_In_Project/popup.js
@@ -8,77 +8,105 @@ function add_Records(url){
 
         }]
     });
-
 }
 
 //function to display links in the chrome ext window
 function showLinks() {
+    for (var i = 0; i < links.length; i++) {
+        //create download image and download functionality
+        var download = document.createElement("img");
+        download.title = "Download this video";
+        download.src = 'download.png';
+        download.setAttribute("url", links[i].url);
+        download.onclick = function(event) {
+            chrome.downloads.download({
+                url: event.target.getAttribute("url"),
+            });
+            console.log(event.target.getAttribute("url"));
+            add_Records(event.target.getAttribute("url"))
+        };
+        //create collumns for name and download links
+        var colDownload = document.createElement("td");
+        colDownload.className += "download";
+        colDownload.appendChild(download);
+        var colName = document.createElement("td");
+        colName.innerText = links[i].name;
 
-  for (var i = 0; i < links.length; i++) {
-    //create download image and download functionality
-    var download = document.createElement("img");
-    download.title = "Download this video";
-    download.setAttribute("url", links[i].url);
-    download.onclick = function(event) {
-      
-      chrome.downloads.download({
-        url: event.target.getAttribute("url"),
-      });
-      console.log(event.target.getAttribute("url"));
-      add_Records(event.target.getAttribute("url"))
+        //append collumns to the row
+        var rows = document.createElement("tr");
+        rows.appendChild(colDownload);
+        rows.appendChild(colName);
 
-    };
-    //create collumns for name and download links
-    
-    var colDownload = document.createElement("td");
-    colDownload.className += "download"
-    colDownload.appendChild(download);
-
-    var colName = document.createElement("td");
-    colName.innerText = links[i].name;
-
-    //append collumns to the row
-    var rows = document.createElement("tr");
-    rows.appendChild(colDownload);
-    rows.appendChild(colName);
-
-    var linksDisplay = document.getElementById("links");
-    linksDisplay.appendChild(rows);
-  }
+        var linksDisplay = document.getElementById("links");
+        linksDisplay.appendChild(rows);
+    }
 }
+
+//function used by the download all button
+function downloadAll(){
+    for (var i = 0; i < links.length; i++) {
+
+        var download = document.createElement("tr");
+        download.setAttribute("url", links[i].url);
+
+        chrome.downloads.download({
+            url: download.getAttribute("url"),
+        });
+          console.log(download.getAttribute("url"));
+          add_Records(download.getAttribute("url"))
+    };
+}
+
 
 // display videos upon clicking button
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("hiiiiiiiiiii");
-        var checkPageButton = document.getElementById("checkPage");
+    var checkPageButton = document.getElementById("checkPage");
     if(checkPageButton){
-    checkPageButton.addEventListener("click", function() {
-        chrome.extension.onRequest.addListener(function(vidData){
-            for (var i in vidData) {
-                links.push(vidData[i]);
-            }
-            showLinks();
-        });
-        chrome.windows.getCurrent(function (currentWindow) {
-            chrome.tabs.query({active: true, windowId: currentWindow.id}, function(activeTabs) {
-                chrome.tabs.executeScript(activeTabs[0].id, {file: "video_urls.js"});
+        checkPageButton.addEventListener("click", function() {
+            chrome.extension.onRequest.addListener(function(vidData){
+                for (var i in vidData) {
+                    links.push(vidData[i]);
+                }
+                showLinks();
+            });
+            chrome.windows.getCurrent(function (currentWindow) {
+                chrome.tabs.query({active: true, windowId: currentWindow.id}, function(activeTabs) {
+                    chrome.tabs.executeScript(activeTabs[0].id, {file: "video_urls.js"});
+                });
             });
         });
-    });
-}
-
+    }
     var hist = document.getElementById("history");
-if(hist){
-    hist.addEventListener("click",function(){
-    console.log('history button clicked')
-    chrome.runtime.sendMessage({
-        message: 'get_records'
+    if(hist){
+        hist.addEventListener("click",function(){
+            console.log('history button clicked')
+            chrome.runtime.sendMessage({
+                message: 'get_records'
+            });
         });
-    });
-}
+    }
 })
 
-//
+//download all button
+document.addEventListener("DOMContentLoaded", function() {
+    var downloadAllButton = document.getElementById("downloadAll");
+    if(downloadAllButton){
+        downloadAllButton.addEventListener("click", function() {
+            chrome.extension.onRequest.addListener(function(vidData){
+                for (var i in vidData) {
+                    links.push(vidData[i]);
+                }
+                downloadAll();
+            });
+            chrome.windows.getCurrent(function (currentWindow) {
+                chrome.tabs.query({active: true, windowId: currentWindow.id}, function(activeTabs) {
+                    chrome.tabs.executeScript(activeTabs[0].id, {file: "video_urls.js"});
+                });
+            });
+        });
+    }
+})
+
 
 
 

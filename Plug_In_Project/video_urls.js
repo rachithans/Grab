20e1a4513b91f5c04b0ee4b83a68f3f5320159b4
Yinhao Cui <yn387824@dal.ca>
2021-07-23 23:30:16 +0800

    add can download picture and audio but is all show in avalible link, and not finshed popup notification

--- Plug_In_Project/video_urls.js
+++ Plug_In_Project/video_urls.js
@@ -1,23 +1,33 @@
 var videoType = ["flv", "avi", "mkv", "mov", "mp4", "mpeg", "mpg","mts",
-  "mxf", "ogv", "vep", "vob", "webm", "wlmp", "wmv", "rm", "swf","jpg","png","jpeg","gif"];
+  "mxf", "ogv", "vep", "vob", "webm", "wlmp", "wmv", "rm", "swf","jpg","png","jpeg","gif","mp3"];
+
+var pictureType =["jpg","png","jpeg","gif"]
 
 //convert webpage html to single string
 var htmlText = document.body.innerHTML.toString();
 
 var videoRegex = videoType.join("|");
 
+var pictureRegex = pictureType.join("|");
+
 // pattern to match urls that point to videos
 //code adapted from  https://github.com/dimofte/chrome-video-downloader/blob/master/scrape_video_urls.js
 var urlRegex  = new RegExp('("|\')(http|https):\\/\\/[^("|\'|,)]*?\\.(' +videoRegex +')[^("|\')]*?("|\')','gi');
 
 
+var picurlRegex  = new RegExp('("|\')(http|https):\\/\\/[^("|\'|,)]*?\\.(' +videoRegex +')[^("|\')]*?("|\')','gi');
+
 // Build the array of video urls
 var urls = htmlText.match(urlRegex);
+var picurls = htmlText.match(picurlRegex);
 // remove quotes from strings
 urls = urls.map(function(quotes) {
-      return quotes.substr(1, quotes.length-2)
+    return quotes.substr(1, quotes.length-2)
     });
-  
+
+picurls = picurls.map(function(quotes) {
+    return quotes.substr(1, quotes.length-2)
+});    
 var fileRegex = new RegExp('[^\/]+?\.(' + videoRegex + ')');
     
 var vidData = urls.map(function(url) {
